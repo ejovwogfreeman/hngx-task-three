1a4e9636fd02b4d19de67d775a42cd827b70c0ca
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-09-20 16:59:44 +0100

    message

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import loader from "../images/loading.gif";
+import loadingImage from "../images/loading.gif";
 import "../css/Loader.css";
 
 const Loader = () => {
   return (
     <div className="loader">
-      <img src={loader} alt="" />
+      <img src={loadingImage} alt="" />
     </div>
   );
 };
